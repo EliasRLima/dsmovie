b7b1b97944fd989d45dff1f45f4b1df67d564cff
Elias Lima <38722114+EliasRLima@users.noreply.github.com>
2022-03-09 15:31:50 -0300

    correcao-link4

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,14 @@ import {
   Routes,
   Route
 } from "react-router-dom";
-import Listing from './pages/listing';
-import Form from './pages/form';
-import Navbar from './components/navbar';
+import Listing from 'pages/listing';
+import Form from 'pages/form';
+import NavBar from 'components/navbar';
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
+      <NavBar />
       <Routes>
         <Route path="/" element={<Listing />} />
         <Route path="/form">
